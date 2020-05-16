455f4b548e6ed15902b369f0b52620667a1297a2
Quentin <75quentinguerrier@gmail.com>
2020-05-16 02:21:26 +0200

    Create a line of présentation

--- JS/index.js
+++ JS/index.js
@@ -6,11 +6,13 @@ var image = document.getElementById('presentation-picture');
 // scroll
 window.onscroll = function(){
     var scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
-    if (scrollTop > 250 && scrollTop < 1299){
+    if (scrollTop > 250 && scrollTop < 1250){
         image.style.top = (15 / scrollTop) * 800 + "%";
+        image.style.transitionDuration = "50ms";
     }
 
-    if(scrollTop > 1300){
-        image.style.top = - (15 / scrollTop) * 800 + "%";
+    if(scrollTop >= 1250){
+        image.style.top = "-20%";
+        image.style.transitionDuration = "1000ms";
     }
 };
